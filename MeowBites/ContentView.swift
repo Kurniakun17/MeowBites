87b495f82699cb0482d136f01ddb47e82c22c2e3
Kurniakun17 <kurniakharisma17@gmail.com>
2024-06-26 09:57:57 +0700

    Initial Commit

---
+++ MeowBites/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeowBites
+//
+//  Created by Kurnia Kharisma Agung Samiadjie on 26/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeowBites/MeowBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeowBitesApp.swift
+//  MeowBites
+//
+//  Created by Kurnia Kharisma Agung Samiadjie on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MeowBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
